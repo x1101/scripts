13a8ec81250c31a57c5805299166a62587fa2a4a
Your full name <x1101@gmx.com>
2015-11-12 16:51:14 -0500

    Added album art grabbing

--- getTT.sh
+++ getTT.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 ######
-#	Apparently Nathan Lowells books are coming off podiobooks
-#	Since that sucks, I want them saved somewhere
+#	Apparenlty Nathan Lowells books are coming off podiobooks
+#	Since that sucks, I want them
 ######
 
-## List of the podcasts RSS Feeds
 podcastlist=(
 http://podiobooks.com/rss/feeds/episodes/trader-tales-1-quarter-share/
 http://podiobooks.com/rss/feeds/episodes/trader-tales-2-half-share/
@@ -16,12 +15,13 @@ http://podiobooks.com/rss/feeds/episodes/trader-tales-6-owners-share/
 )
 
 for podcast in ${podcastlist[@]}; do
-	## Make a dir to hold the podcasts
 	dir=`echo ${podcast}|awk -F- '{print "0"$3" - "$4,$5}'|sed 's/\///g'|sed -r 's/\b(.)/\U\1/g'`
 	mkdir "${dir}"
 	echo "Doing ${dir}"
 	cd "${dir}"
-	## And get all the episodes
+	## Get the art
+	image=`wget -q -O - ${podcast} |sed 's/<\//\n<\//g' |grep image|grep jpg|awk -F= '{print $2}'|sed -e 's/"//g' -e 's/>//'`
+	wget -q ${image}
 	episodes=(`wget -q -O - ${podcast} | sed 's/<\//\n<\//g'|grep enclosure|egrep -v '(itunes|donate|Outro)'|awk '{print $2}'|awk -F= '{print $2}'|sed 's/"//g'`)
 	for ep in ${episodes[@]}; do
 		echo "Getting ${ep}"
