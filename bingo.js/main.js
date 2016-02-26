7f5c37a07fb2f030fa28b3096f5a0af4f0496e6d
x1101 <x1101@x1101.net>
2016-02-25 22:35:50 -0500

    Functional code, needs some CSS to make the output look less like shit

--- bingo.js/main.js
+++ bingo.js/main.js
@@ -1,13 +1,52 @@
-function submit ()
+function randInt (min, max)
 {
-  console.log("Called submit");
-  var cols = document.getElementById('cols').value;
-  var rows = document.getElementById('rows').value;
-  var werdsArray = document.getElementById('wordlist').value.split("\n");
-  //window.alert("Rows: " + rows + "\nCols: " + cols);
-  //document.getElementById("res").innerHTML = rows + "x" + cols + "<br />List:<br />" + werds;
-  for (i=0; i <= werdsArray.length; i++)
-  {
-    console.log(werdsArray[i]);
+  return Math.floor(Math.random() * (max - min + 1)) + min;  
+}
+
+function areaSize()
+{
+  var txtStart = '<textarea id="wordlist" rows=';
+  var txtEnd = '></textarea><br /><input type=button value="Submit" onclick="mkTable()"/><br />';
+  document.getElementById("txt").innerHTML = txtStart + numEntries() + txtEnd;
+}
+
+function numEntries()
+{
+  cols = document.getElementById('cols').value;
+  rows = document.getElementById('rows').value;  
+  return rows*cols;
+}
+
+/*
+  HTML Tables are awful, but I can't think of another way to do this 
+  If there's a bette way, please submit a pull reuqest with something else
+*/
+function mkTable ()
+{
+  var contents = '<table>';
+  var werds = document.getElementById('wordlist').value.split("\n");
+  var len = werds.length;
+  // fail if we have less entires than we need
+  if (len < numEntries()) 
+  {  
+    alert("Not enough words to make that board");
+    areaSize();
+    return ;
   }
+  var remLen = len;
+
+  for (i=0; i < rows; i++)
+  {
+    contents += '<br />\n<tr>\n';
+    for (j=0; j < cols; j++)
+    {
+          werd = werds[randInt(0,remLen -1)];
+          contents+='<td>' + werd + "</td>";
+          werds.splice(werds.indexOf(werd),1);
+          remLen --;
+    }
+    contents += '\n</tr>';
+  }
+  contents += '\n</table>';
+  document.getElementById("table").innerHTML = contents;
 }
